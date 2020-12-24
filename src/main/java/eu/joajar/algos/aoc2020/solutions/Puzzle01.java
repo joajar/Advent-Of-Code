a832e1da205655d90b0b58765f6229c67a8934ea
joajar <joanna.z.jaroszewska@gmail.com>
2020-12-24 19:05:55 +0100

    Solutions for day 1 puzzles refactored

--- src/main/java/eu/joajar/algos/aoc2020/solutions/Puzzle01.java
+++ src/main/java/eu/joajar/algos/aoc2020/solutions/Puzzle01.java
@@ -2,6 +2,7 @@ package eu.joajar.algos.aoc2020.solutions;
 
 import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Uses the assumption that the data doesn't contain any duplicates.
@@ -20,19 +21,14 @@ public class Puzzle01 extends AbstractPuzzleDataReader {
     @Override
     public String solveFirstPart() {
         try {
-            return Arrays.stream(getData())
+            return parse(
+                Arrays.stream(getData())
                     /* Finds the stream of sets of pairs of elements (without repetitions) from the original data. */
                     .flatMap(s1 -> Arrays.stream(getData())
-                            .filter(s2 -> s1.compareTo(s2) > 0)
-                            .map(s2 -> Set.of(s1, s2))
+                        .filter(s2 -> s1.compareTo(s2) > 0)
+                        .map(s2 -> Set.of(s1, s2))
                     )
-                    .filter(set -> set.stream().mapToInt(Integer::parseInt).sum() == 2020)
-                    .findAny()
-                    .orElseThrow(IllegalArgumentException::new)
-                    .stream()
-                    .reduce((a, b) -> String.valueOf(Integer.parseInt(a) * Integer.parseInt(b)))
-                    .orElseThrow(IllegalArgumentException::new)
-                    ;
+            );
         } catch (IllegalArgumentException e) {
             return "Unable to solve the puzzle!";
         }
@@ -41,25 +37,31 @@ public class Puzzle01 extends AbstractPuzzleDataReader {
     @Override
     public String solveSecondPart() {
         try {
-            return Arrays.stream(getData())
+            return parse(
+                Arrays.stream(getData())
                     /* Finds the stream of sets of triplets of elements (without repetitions) from the original data. */
                     .flatMap(s1 -> Arrays.stream(getData())
-                            .filter(s2 -> s1.compareTo(s2) > 0)
-                            .flatMap(s2 -> Arrays.stream(getData())
-                                    .filter(s3 -> s2.compareTo(s3) > 0)
-                                    .map(s3 -> Set.of(s1, s2, s3))
-                            )
+                        .filter(s2 -> s1.compareTo(s2) > 0)
+                        .flatMap(s2 -> Arrays.stream(getData())
+                            .filter(s3 -> s2.compareTo(s3) > 0)
+                            .map(s3 -> Set.of(s1, s2, s3))
+                        )
                     )
-                    .filter(set -> set.stream().mapToInt(Integer::parseInt).sum() == 2020)
-                    .findAny()
-                    .orElseThrow(IllegalArgumentException::new)
-                    .stream()
-                    .reduce((a,b)->String.valueOf(Integer.parseInt(a) * Integer.parseInt(b)))
-                    .orElseThrow(IllegalArgumentException::new)
-                    ;
+            );
         } catch (IllegalArgumentException e) {
             return "Unable to solve the puzzle!";
         }
+    }
+
+    private String parse(Stream<Set<String>> stream) throws IllegalArgumentException {
+        return stream
+            .filter(set -> set.stream().mapToInt(Integer::parseInt).sum() == 2020)
+            .findAny()
+            .orElseThrow(IllegalArgumentException::new)
+            .stream()
+            .reduce((a, b) -> String.valueOf(Integer.parseInt(a) * Integer.parseInt(b)))
+            .orElseThrow(IllegalArgumentException::new)
+            ;
     }
 }
 
